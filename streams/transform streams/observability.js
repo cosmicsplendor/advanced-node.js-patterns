87774bd097cd12ede6202850992b804547cf82b1
Umexo <defiantextropia@gmail.com>
2020-11-14 13:26:15 +0545

    Use PassThroughSteream to implement observability for data flowing through the stream

---
+++ streams/transform streams/observability.js
@@ -0,0 +1,17 @@
+const { PassThrough } = require("stream")
+const getRandStr = require("../utils/getRandStr")
+
+const monitor = new PassThrough()
+
+totalBytes = 0
+monitor.on("data", chunk => {
+    console.log(chunk)
+    totalBytes += chunk.toString().length
+})
+monitor.on("finish", () => {
+    console.log(`Bytes read: ${totalBytes}`)
+})
+while (Math.random() > 0.1) {
+    monitor.write(getRandStr(10))
+}
+monitor.end()
