75af525e76191bdcb298d32c3693c4fe31c95917
xtropia <ufocalypse@gmail.com>
2021-01-31 15:49:01 +0545

    Comment docker commands for building a container and running it

--- scalability and architecture/containerization/app.js
+++ scalability and architecture/containerization/app.js
@@ -3,6 +3,8 @@ import { hostname } from "os"
 
 const version = 1
 
+// docker build -tag <tag name> .
+// docker run --it -p <host_port>:<container_port> <tag or hash>
 const server = createServer((req, res) => {
     let i = 0; while (i < 1e7) { i++ }
     res.end(`${hostname()} (v${version})`)
